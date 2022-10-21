62e99382489d6f8c54dba0c47530c8522dfb5e6c
mhhasanmehedi <mmmehedi53@gmail.com>
2022-10-21 21:38:38 +0600

    Social rest api done

--- route/posts.js
+++ route/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // create a post
 router.post("/", async (req, res) => {
@@ -72,13 +73,11 @@ router.get("/timeline/all", async (req, res) => {
   try {
     const currentUser = await User.findById(req.body.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
-    console.log(userPosts);
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
         return Post.find({ userId: friendId });
       })
     );
-
     res.json(userPosts.concat(...friendPosts));
   } catch (err) {
     res.status(500).json(err);
